9bb1ecab509e79707bcde06538bf748be7303629
senashino <phwn26@gmail.com>
2025-03-22 02:18:00 +0700

    update

---
+++ Lab14_Ex04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+
+    protected Shape()
+    {
+        Console.WriteLine("This is some shape with unknown side");
+    }
+
+    public Shape(int NumOfSide)
+    {
+        Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base() 
+    {
+        Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4)
+    {
+        Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3)
+    {
+        Console.WriteLine("This is a triangle");
+    }
+}
---
+++ Lab14_Ex05/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+
+    protected Shape()
+    {
+    }
+
+    public Shape(int NumOfSide) : this()
+    {
+        this.NumOfSide = NumOfSide;
+        Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base(0)
+    {
+        Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4)
+    {
+        Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3) 
+    {
+        Console.WriteLine("This is a triangle");
+    }
+}
